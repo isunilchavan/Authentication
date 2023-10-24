d4678d8b27de3316549210bdd9e5dcba83409cef
Sunil Chavan <139366162+isunilchavan@users.noreply.github.com>
2023-10-24 17:53:48 +0530

    Sign-Up

---
+++ AuthForm.js
@@ -0,0 +1,93 @@
+import { useState, useRef } from "react";
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false); // Added loading state
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true); // Set loading state to true
+
+    if (isLogin) {
+      // Handle login logic
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBT973fCDNn8x1looa46PWlQ-dv2mK1p7k",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          setIsLoading(false);
+          if (res.ok) {
+            // Handle successful sign-up
+          } else {
+            return res.json().then((data) => {
+             let errorMessage= 'Authentication failed' ;
+             if(data&& data.error && data.error.message) {
+              errorMessage = data.error.message;
+             }
+             alert(errorMessage);
+            });
+          }
+        })
+        .finally(() => {
+          setIsLoading(false); // Set loading state to false when request is complete
+        });
+    }
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordInputRef} />
+        </div>
+        <div className={classes.actions}>
+          {isLoading ? ( // Render loader if isLoading is true
+            <p>Sending request...</p>
+          ) : (
+            <button type="submit">
+              {isLogin ? "Login" : "Sign Up"}
+            </button>
+          )}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
